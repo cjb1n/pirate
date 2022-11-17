71670068a0d310d7bff8304fc70e8ef5ac458825
cjb1n <wjdqls8062@gmail.com>
2022-11-17 13:49:51 +0900

    1117commit

---
+++ spring-boot-data-jpa-mysql-master/vue-3-crud-master/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <router-link to="/" class="navbar-brand">AWeSome</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/tutorials" class="nav-link">Tutorials -> 회원리스트</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/add" class="nav-link">add -> 회원가입</router-link>
+        </li>
+      </div>
+    </nav>
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
